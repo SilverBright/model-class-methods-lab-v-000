3ec33f52d3b69ea41e2b25ebcf8474ff1ef76041
Barbara Washington <barbara.m.washington@gmail.com>
2018-09-10 23:16:09 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,6 +1,8 @@
 class Captain < ActiveRecord::Base
   has_many :boats
   
-  
+  def self.catamaran_operators
+    Captain.includes(boats: :classifications).where(classifications: {name: "Catamaran"})
+  end
   
 end
