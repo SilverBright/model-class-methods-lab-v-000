f3dd7a7eeae8204ffc3f8a94c5043d5462f5f2a2
Barbara Washington <barbara.m.washington@gmail.com>
2018-09-10 23:23:36 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -10,7 +10,7 @@ class Captain < ActiveRecord::Base
   end
   
   def self.talented_seafarers
-    where("id IN (?)", self.sailors.pluck(:id) & self.sailboats.pluck(:id))
+    Captain.includes("id IN (?)", self.sailors.pluck(:id) & self.sailboats.pluck(:id))
   end
   
 end
